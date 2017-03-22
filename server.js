69207f17326d017e741dafc32bdfb12f0f0e685b
fresh5447 <doug@kosmojo.com>
2017-03-21 21:32:20 -0600

    configured basic web server using HTTP node module

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("Hello, world!");
+var http = require('http');
+
+http.createServer(function(request, response){
+  response.end('hello, world!');
+  console.log("You server is running on port 3000");
+}).listen(3000);
