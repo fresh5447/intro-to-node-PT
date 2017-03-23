89e68aba762f045577420c79a007ade66813e8f7
fresh5447 <doug@kosmojo.com>
2017-03-22 21:08:46 -0600

    implement GET route to send back a greeting

---
+++ README.md
@@ -0,0 +1,1 @@
+A node project must have a package.json... You can create one manually, but the easiest way to add one to your project is to run the command `npm init`
--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(request, response){
-  response.end('hello, world!');
-  console.log("You server is running on port 3000");
-}).listen(3000);
+
+// next we are going to commit our code, but first..
+// update your readme to explain what this app.get() method
+// is doing..
+// GET our HTTP method
+// '/greeting' the location we need to go to make a request
+// req, res client makes request, server sends response..
+
+app.get('/greeting', function(req, res){
+  res.send("You found the greeting endpoint!");
+});
+
+app.get('/happy', function(req, res){
+  res.send("Its a beautiful day!!")
+});
+
+app.get('/sad', function(req, res){
+  res.send("Its beautiful day - U2")
+});
+// make two new endpoints... one that sends a happy response..
+// and one the sends back a sad response.
+
+var server = app.listen(3000, function(){
+  console.log("App 🔥🔥d up on port 3000")
+});
